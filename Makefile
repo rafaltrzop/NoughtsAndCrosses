8024422a9f804fe9ef32a35d169a3feea417443f
Rafal Trzop <rafaltrzop@gmail.com>
2014-07-02 19:28:07 +0200

    Separate functions from main.c by adding functions.c and functions.h. Update Makefile.

--- Makefile
+++ Makefile
@@ -3,10 +3,13 @@ CFLAGS = -std=c99
 
 all: bin/NoughtsAndCrosses.exe
 
-bin/NoughtsAndCrosses.exe: obj/main.o
+bin/NoughtsAndCrosses.exe: obj/main.o obj/functions.o
 	$(CC) $^ -o $@
 
 obj/main.o: src/main.c
+	$(CC) $(CFLAGS) -c $^ -o $@
+
+obj/functions.o: src/functions.c
 	$(CC) $(CFLAGS) -c $^ -o $@
 
 clean:
---
+++ src/functions.h
@@ -0,0 +1,19 @@
+#ifndef HEADER_H
+    #define HEADER_H
+    #define OPTION_MAX_LENGTH 40
+
+    void printLogo(void);
+    void printGameboard(char board[]);
+    void menuSelection(int * choice, char * title, char options[][OPTION_MAX_LENGTH], int numberOfOptions);
+
+    void printTitle(char * title);
+    void printMenuOptions(char options[][OPTION_MAX_LENGTH], int numberOfOptions);
+    void askForMenuNumber(int * choice, int numberOfOptions);
+
+    void singleplayer(int difficultyLevel);
+    void multiplayer(void);
+
+    int drawing(void);
+    int anyWinners(char boardState[]);
+    void playAgain(void (*mode)(void));
+#endif
