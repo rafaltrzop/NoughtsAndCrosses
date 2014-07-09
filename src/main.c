8eec1caf24dd332e0ab156ca3bb87b6f73f4edd2
Rafal Trzop <rafaltrzop@gmail.com>
2014-07-09 23:19:51 +0200

    Add normal difficulty level AI

--- src/main.c
+++ src/main.c
@@ -61,7 +61,6 @@ void multiplayer(void)
 
         board[field-1] = i % 2 ? 'O' : 'X';
         printTitle("GAMEBOARD");
-
         checkForEndOfGame(&i, &field, board, multiplayer);
 
         /* prepare view for the next iteration */
@@ -84,14 +83,14 @@ void singleplayer(void)
 
     /* decide who starts the game and print instructions */
     int startingPlayer = drawing();
-    char players[2][78] = {
+    char player[2][78] = {
         "   |   YOU start the game!                                                  |",
         "   |   COMPUTER starts the game!                                            |"
     };
 
     printTitle("INSTRUCTIONS");
     printf("   |   Let's flip a coin:                                                   |\n"
-           "%s\n", players[startingPlayer-1]);
+           "%s\n", player[startingPlayer-1]);
     printGameboard("123456789");
 
     int field;
@@ -138,13 +137,138 @@ void aiGenerateMove(int difficultyLevel, int * field, char board[])
     {
         *field = rand() % 9 + 1; /* use seed from drawing() function */
         if(board[*field-1] != ' ') aiGenerateMove(difficultyLevel, field, board);
-    } else if(difficultyLevel == 2)
+    }
+
+    if(difficultyLevel == 2)
+    {
+        int cantBlock = 1;
+        int i;
+
+        // X X -
+        // Y Y -
+        // Z Z -
+        for(i = 0; i < 7; i += 3)
+            if(board[i] != ' ' && board[i] == board[i+1] && board[i+2] == ' ')
+            {
+                *field = i + 3;
+                cantBlock = 0;
+                break;
+            }
+
+        // - X X
+        // - Y Y
+        // - Z Z
+        for(i = 1; i < 8; i += 3)
+            if(board[i] != ' ' && board[i] == board[i+1] && board[i-1] == ' ')
+            {
+                *field = i;
+                cantBlock = 0;
+                break;
+            }
+
+        // X - X
+        // Y - Y
+        // Z - Z
+        for(i = 0; i < 7; i += 3)
+            if(board[i] != ' ' && board[i] == board[i+2] && board[i+1] == ' ')
+            {
+                *field = i + 2;
+                cantBlock = 0;
+                break;
+            }
+
+        // X Y Z
+        // X Y Z
+        // - - -
+        for(i = 0; i < 3; i++)
+            if(board[i] != ' ' && board[i] == board[i+3] && board[i+6] == ' ')
+            {
+                *field = i + 7;
+                cantBlock = 0;
+                break;
+            }
+
+        // - - -
+        // X Y Z
+        // X Y Z
+        for(i = 3; i < 6; i++)
+            if(board[i] != ' ' && board[i] == board[i+3] && board[i-3] == ' ')
+            {
+                *field = i - 2;
+                cantBlock = 0;
+                break;
+            }
+
+        // X Y Z
+        // - - -
+        // X Y Z
+        for(i = 0; i < 3; i++)
+            if(board[i] != ' ' && board[i] == board[i+6] && board[i+3] == ' ')
+            {
+                *field = i + 4;
+                cantBlock = 0;
+                break;
+            }
+
+        // X - -
+        // - X -
+        // - - -
+        if(board[0] != ' ' && board[0] == board[4] && board[8] == ' ')
+        {
+            *field = 9;
+            cantBlock = 0;
+        }
+
+        // - - -
+        // - X -
+        // - - X
+        if(board[4] != ' ' && board[4] == board[8] && board[0] == ' ')
+        {
+            *field = 1;
+            cantBlock = 0;
+        }
+
+        // X - -
+        // - - -
+        // - - X
+        if(board[0] != ' ' && board[0] == board[8] && board[4] == ' ')
+        {
+            *field = 5;
+            cantBlock = 0;
+        }
+
+        // - - X
+        // - X -
+        // - - -
+        if(board[2] != ' ' && board[2] == board[4] && board[6] == ' ')
+        {
+            *field = 7;
+            cantBlock = 0;
+        }
+
+        // - - -
+        // - X -
+        // X - -
+        if(board[4] != ' ' && board[4] == board[6] && board[2] == ' ')
+        {
+            *field = 3;
+            cantBlock = 0;
+        }
+
+        // - - X
+        // - - -
+        // X - -
+        if(board[2] != ' ' && board[2] == board[6] && board[4] == ' ')
+        {
+            *field = 5;
+            cantBlock = 0;
+        }
+
+        if(cantBlock) aiGenerateMove(1, field, board);
+    }
+
+    if(difficultyLevel > 2)
     {
         //to do
-        *field = 2;
-    } else if(difficultyLevel == 3)
-    {
-        //to do
-        *field = 3;
     }
 }
